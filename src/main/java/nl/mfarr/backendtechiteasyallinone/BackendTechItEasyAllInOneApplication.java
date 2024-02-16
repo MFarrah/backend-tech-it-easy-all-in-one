d64ed79af85419f578eaf45306f684cbb248ef96
MFarr <micfar85@gmail.com>
2024-02-16 19:17:08 +0100

    start opdracht

---
+++ src/main/java/nl/mfarr/backendtechiteasyallinone/BackendTechItEasyAllInOneApplication.java
@@ -0,0 +1,13 @@
+package nl.mfarr.backendtechiteasyallinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechItEasyAllInOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTechItEasyAllInOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/mfarr/backendtechiteasyallinone/BackendTechItEasyAllInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mfarr.backendtechiteasyallinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTechItEasyAllInOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
