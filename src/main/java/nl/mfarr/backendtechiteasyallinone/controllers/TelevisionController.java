88365908610e46715ce6fb54bf4ae476f996d541
MFarr <micfar85@gmail.com>
2024-02-16 19:41:40 +0100

    TelevisionController added

---
+++ src/main/java/nl/mfarr/backendtechiteasyallinone/controllers/TelevisionController.java
@@ -0,0 +1,77 @@
+package nl.mfarr.backendtechiteasyallinone.controllers;
+import nl.mfarr.backendtechiteasyallinone.entities.Television;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequestMapping("/televisions")
+public class TelevisionController {
+    private Long currentId = 1L;
+    private final List<Television> televisionList = new ArrayList<Television>();
+
+    @PostMapping
+    public ResponseEntity<Object> createTelevision(@RequestBody Television television) {
+        television.setId(currentId++);
+        televisionList.add(television);
+        return ResponseEntity.status(HttpStatus.CREATED).body(television);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Television> getTelevision(@PathVariable Long id){
+        var optionalTelevision = findTelevisionById(id);
+        if (optionalTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optionalTelevision);
+    }
+    private Television findTelevisionById(Long id) {
+        for (Television television : televisionList) {
+            if (television.getId().equals(id)) {
+                return television;
+            }
+        }
+        return null;
+    }
+    @GetMapping
+    public ResponseEntity<List<Television>> getAllTelevisions() {
+        List<Television> allTelevisions = getAllTelevisionsFromList();
+
+        if (allTelevisions.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(allTelevisions);
+    }
+
+    private List<Television> getAllTelevisionsFromList() {
+        return televisionList;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Television> updateTelevision(@PathVariable Long id, @RequestBody Television television) {
+        var existingTelevision = findTelevisionById(id);
+        if (existingTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        existingTelevision.setType(television.getType());
+        existingTelevision.setName(television.getName());
+        existingTelevision.setBrand(television.getBrand());
+        existingTelevision.setPrice(television.getPrice());
+        existingTelevision.setOriginalStock(television.getOriginalStock());
+        existingTelevision.setSold(television.getSold());
+        //geen idee hoe ik de Options lijst update feature zou moeten schrijven. Want deze bevat een lijst van objecten.//
+        return ResponseEntity.ok(existingTelevision);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Television> deleteTelevision(@PathVariable Long id) {
+        var existingTelevision = findTelevisionById(id);
+        if (existingTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        televisionList.remove(existingTelevision);
+        return ResponseEntity.noContent().build();
+    }
+}
